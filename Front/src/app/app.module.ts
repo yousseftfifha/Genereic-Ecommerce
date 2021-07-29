0362480ab9d848e13ebfd3f4a0291d78ff64022c
yousseftfifha <youssef.tfifha@esprit.tn>
2021-07-29 14:52:12 +0100

    Setting NG

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Template/navbar/navbar.component';
+import { SidebarComponent } from './Template/sidebar/sidebar.component';
+import { FooterComponent } from './Template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
